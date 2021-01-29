74a8267dcb033518dbb0acb54a6532653c9de197
mygithubaccount333 <alozie4God@gmail.com>
2021-01-29 03:29:21 -0800

    worked on the login

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "./Login.css"
 import { Button } from "@material-ui/core"
+import { auth, provider } from "./firebase"
 
 function Login() {
+
+    const signIn = () => {
+        auth
+        .signInWithPopup(provider)
+        .then((result) => console.log(result))
+        .catch(error => alert(error.message))      
+
+    }
+
     return (
         <div className="login">
             <div className="login__container">
-                <img alt="" src="https://i.pinimg.com/564x/1f/0a/7c/1f0a7cd394abb65aa8088f59be819466.jpg"  />
+                <img alt="" src="https://i.pinimg.com/564x/1f/0a/7c/1f0a7cd394abb65aa8088f59be819466.jpg"/>
                 <div className="login__text">
                     <h1>Sign in to WhatsApp</h1>
                 </div>
-                <Button type="submit">sign in with Google</Button>
+                <Button type="submit" onClick={signIn}>sign in with Google</Button>
 
             </div>
         </div>
